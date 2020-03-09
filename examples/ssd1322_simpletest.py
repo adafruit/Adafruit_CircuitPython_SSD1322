ba434f1670869a8358f55a6a6f0fe17a586c5fc0
dherrada <dylan.herrada@gmail.com>
2020-03-09 19:27:51 -0400

    Ran black, updated to pylint 2.x

--- examples/ssd1322_simpletest.py
+++ examples/ssd1322_simpletest.py
@@ -12,7 +12,8 @@ tft_cs = board.D6
 tft_dc = board.D9
 tft_reset = board.D5
 
-display_bus = displayio.FourWire(spi, command=tft_dc, chip_select=tft_cs,
-                                 reset=tft_reset, baudrate=1000000)
+display_bus = displayio.FourWire(
+    spi, command=tft_dc, chip_select=tft_cs, reset=tft_reset, baudrate=1000000
+)
 time.sleep(1)
 display = adafruit_ssd1322.SSD1322(display_bus, width=256, height=64, colstart=28)
